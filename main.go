6e63d968b35562ce061f1dddacfc5adfbd7f5023
drbh <david.richard.holtz@gmail.com>
2019-12-07 11:02:22 -0500

    add cli input

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	// "encoding/json"
 	"fmt"
+	"os"
 
 	messagestats "github.com/drbh/imessage-stats/go-text-statistics"
 )
 
 func main() {
 
-	allstats := messagestats.GetFullProfileStats("+13478344775")
+	argsWithoutProg := os.Args[1:]
+
+	var number string
+	if len(argsWithoutProg) > 0 {
+		number = argsWithoutProg[0]
+	} else {
+		os.Exit(0)
+	}
+
+	allstats := messagestats.GetFullProfileStats(number)
 	fmt.Println(string(allstats))
 }
